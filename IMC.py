bb917ca6068ff4dad7c7da0082c17695ee6b5437
Cintia <cintiasrodrigues2107@gmail.com>
2026-03-11 14:13:20 -0300

    Inclusao do IMC

---
+++ IMC.py
@@ -0,0 +1,18 @@
+#Código para executar o cálculo do IMC e dar o resultado em tela:
+
+nome = input("Digite o nome: ")
+peso = float(input("Informe o pedo em Kg: "))
+altura = float(input("Informe a altura em metrps (ex. 1.75): "))
+
+imc = peso / (altura ** 2)
+print(f"{nome} , O seu IMC é: {imc: .2f}")
+
+baixo_peso = imc < 18.5
+peso_normal = 18.5 <= imc < 25
+sobrepeso = 25 <= imc < 30
+obesidade = imc >= 30
+
+print(baixo_peso)
+print(peso_normal)
+print(sobrepeso)
+print(obesidade)
