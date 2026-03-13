2eec162fd9754b3b3e7ad8035fbbe7d24b96ccd9
Cintia <cintiasrodrigues2107@gmail.com>
2026-03-13 19:31:04 -0300

    atualizacoes

--- Exercícios operadores de atribuição/Exercícios operadores de atribuição/27.py
+++ Exercícios operadores de atribuição/Exercícios operadores de atribuição/27.py
@@ -1,4 +1,7 @@
 # Leia num (float). Faça num **= 0.5 (raiz quadrada), depois num **= 2. Mostre cada etapa.
 num = float(input("Qual é o número: "))
 num **= 0.5
-print("")
+print("Qual raiz quadrada do n: " , num)
+
+num **= 2
+print("Qual valor novo: " , num)
